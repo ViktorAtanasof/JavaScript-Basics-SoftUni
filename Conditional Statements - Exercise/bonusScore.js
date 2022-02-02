1567758c3f00d7df6d1628ad02aa6342f9a6c2d7
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-02-02 16:03:46 +0200

    Update and rename Bonus Score to bonusScore.js

--- Conditional Statements - Exercise/bonusScore.js
+++ Conditional Statements - Exercise/bonusScore.js
@@ -5,18 +5,18 @@ function bonusScore(input) {
     if (points <= 100) {
         bonus = 5;
     } else if (points > 1000) {
-        bonus = points * 0.10
+        bonus = points * 0.10;
     } else if (points > 100) {
-        bonus = points * 0.20
+        bonus = points * 0.20;
     } 
     if (points % 2 === 0) {
-        bonus = bonus + 1
+        bonus = bonus + 1;
     } else if (points % 10 === 5) {
-        bonus = bonus + 2
+        bonus = bonus + 2;
     }
 
     console.log(bonus);
     console.log(points + bonus);
 
 }
-bonusScore(["2703"])
+bonusScore(["2703"]);
