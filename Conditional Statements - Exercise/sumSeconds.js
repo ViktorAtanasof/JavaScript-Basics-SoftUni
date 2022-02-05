7ed6f130db8c96763f8074b55547282e08b341f8
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-02-05 16:55:01 +0200

    Update and rename Sum Seconds to sumSeconds.js

--- Conditional Statements - Exercise/sumSeconds.js
+++ Conditional Statements - Exercise/sumSeconds.js
@@ -2,10 +2,10 @@ function sumSeconds(input) {
     let timeFirst = Number(input[0]);
     let timeSecond = Number(input[1]);
     let timeThird = Number(input[2]);
-    let totalTime = timeFirst + timeSecond + timeThird
+    let totalTime = timeFirst + timeSecond + timeThird;
 
     let minutes = Math.floor(totalTime / 60);
-    let seconds = totalTime % 60
+    let seconds = totalTime % 60;
 
     if (seconds < 10) {
         console.log(`${minutes}:0${seconds}`);
@@ -14,4 +14,5 @@ function sumSeconds(input) {
     }
 
 }
-sumSeconds(["14", "12", "10"])
+
+sumSeconds(["14", "12", "10"])'
