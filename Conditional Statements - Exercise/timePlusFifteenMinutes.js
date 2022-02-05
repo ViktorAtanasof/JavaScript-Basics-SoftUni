6cf8de2e1f1ec95deca208e54ad32cdd0df05457
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-02-05 16:56:33 +0200

    Update and rename Time +15 Minutes to timePlusFifteenMinutes.js

--- Conditional Statements - Exercise/timePlusFifteenMinutes.js
+++ Conditional Statements - Exercise/timePlusFifteenMinutes.js
@@ -1,22 +1,24 @@
-function time15Minutes(input) {
+function timePlusFifteenMinutes(input) {
     let hour = Number(input[0]);
     let minutes = Number(input[1]);
     minutes = minutes + 15;
 
     if (minutes > 59) {
-        hour = hour + 1
+        hour = hour + 1;
     }
     if (minutes > 59) {
-        minutes = minutes - 60
+        minutes = minutes - 60;
     }
     if (hour > 23) {
-        hour = 0
+        hour = 0;
     }
 
     if (minutes < 10) {
         console.log(`${hour}:0${minutes}`);
-    } else (console.log(`${hour}:${minutes}`))
+    } else {
+        (console.log(`${hour}:${minutes}`));
+    }
 
 }
 
-time15Minutes(["0", "44"])
+time15Minutes(["0", "44"]);
