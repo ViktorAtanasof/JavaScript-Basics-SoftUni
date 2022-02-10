9e25b74a77c61d140b812fae2921e9e8b3b84adb
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-02-10 17:05:30 +0200

    Update and rename Hotel Room to hotelRoom.js

--- Conditional Statements Advanced - Exercise/hotelRoom.js
+++ Conditional Statements Advanced - Exercise/hotelRoom.js
@@ -1,48 +1,49 @@
 function hotelRoom(input) {
-    let month = input[0]
-    let nights = Number(input[1])
+    let month = input[0];
+    let nights = Number(input[1]);
     let price;
     let price2;
 
     if (month == "May" || month == "October" && nights > 7) {
-        price = 50
+        price = 50;
     }
     else if (month == "June" || month == "September") {
-        price = 75.20
+        price = 75.20;
     }
     else if (month == "July" || month == "August") {
-        price = 76
+        price = 76;
     }
 
     if (month == "May" || month == "October") {
-        price2 = 65
+        price2 = 65;
     }
     else if (month == "June" || month == "September") {
-        price2 = 68.70
+        price2 = 68.70;
     }
     else if (month == "July" || month == "August") {
-        price2 = 77
+        price2 = 77;
     }
 
     if (nights > 7 && nights <= 14 && (month == "May" || month == "October")) {
-        price = price * 0.95
+        price = price * 0.95;
     }
     else if (nights > 14 && (month == "May" || month == "October")) {
-        price = price * 0.70
+        price = price * 0.70;
     }
     else if (nights > 14 && (month == "June" || month == "September")) {
-        price = price * 0.80
+        price = price * 0.80;
     }
 
     if (nights > 14) {
-        price2 = price2 * 0.90
+        price2 = price2 * 0.90;
     }
 
-    let price3 = price * nights
-    let price4 = price2 * nights
+    let price3 = price * nights;
+    let price4 = price2 * nights;
 
-    console.log(`Apartment: ${price4.toFixed(2)} lv.`)
-    console.log(`Studio: ${price3.toFixed(2)} lv.`)
+    console.log(`Apartment: ${price4.toFixed(2)} lv.`);
+    console.log(`Studio: ${price3.toFixed(2)} lv.`);
 
 }
-hotelRoom(["May","15"])
+
+hotelRoom(["May","15"]);
