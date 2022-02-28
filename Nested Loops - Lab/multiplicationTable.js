e498b875e6d2175448d5efe5647e482a258d9c68
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-02-28 15:50:10 +0200

    Update and rename Multiplication Table to multiplicationTable.js

--- Nested Loops - Lab/multiplicationTable.js
+++ Nested Loops - Lab/multiplicationTable.js
@@ -3,10 +3,11 @@ function multiplicationTable() {
     for (let firstNum = 1; firstNum <= 10; firstNum++) {
         for (let secondNum = 1; secondNum <= 10; secondNum++) {
             for (let thirdNum = 1; thirdNum <= 10; thirdNum++) {
-                let result = firstNum * secondNum
+                let result = firstNum * secondNum;
                 console.log(`${firstNum} * ${secondNum} = ${result}`);
             }
         }
     }
 }
+
 multiplicationTable(['222']);
