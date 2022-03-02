e863c94f553d6afd2d3dfae647c953abb65c89f8
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-03-02 12:53:59 +0200

    Update and rename Walking to walking.js

--- While Loop - Exercise/walking.js
+++ While Loop - Exercise/walking.js
@@ -30,11 +30,12 @@ function walking(input) {
         console.log(`Goal reached! Good job!`);
         console.log(`${diff} steps over the goal!`);
     } else {
-        console.log(`${diff} more steps to reach goal.`)
+        console.log(`${diff} more steps to reach goal.`);
     }
 
 }
+
 walking(["1000",
     "1500",
     "2000",
-    "6500"])
+    "6500"]);
